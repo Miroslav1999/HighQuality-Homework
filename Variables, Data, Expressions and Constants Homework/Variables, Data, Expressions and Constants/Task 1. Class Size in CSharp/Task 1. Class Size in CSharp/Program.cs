85968b5d2ae7cb6b3f525346f1aab2f8f84918fc
Miroslav IT <miro.vasilev99@gmail.com>
2015-09-03 21:03:25 +0300

    Variables, Data, Expressions and Constant

---
+++ Variables, Data, Expressions and Constants Homework/Variables, Data, Expressions and Constants/Task 1. Class Size in CSharp/Task 1. Class Size in CSharp/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace Task_1.Class_Size_in_CSharp
+{
+    using System;
+
+    public class Size
+    {
+        private double width;
+        private double height;
+
+        public Size(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Height
+        {
+            get 
+            {
+                return this.Height;
+            }
+
+            private set 
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height have to be a positive double value");
+                }
+
+                this.Height = value;
+            }
+        }
+
+        public double Width 
+        {
+            get 
+            {
+                return this.Width;
+            }
+
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width have to be a positive double value");
+                }
+
+                this.Width = value;
+            }
+        }
+
+        public static Size GetRotatedSize(Size s, double angleOfTheFigureThatWillBeRotaed)
+        {
+            double oldWidth = oldSize.Width;
+            double oldHeight = oldHeight.Height;
+
+            var cos = Math.Cos(rotationAngel);
+            var sin = Math.Sin(rotationAngel);
+
+            var newWidth = Math.Abs((cos * oldWidth) + (sin * oldHeight));
+            var newHeight = Math.Abs((sin * oldWidth) + (cos * oldHeight));
+
+            var roatedSize = new Size(newWidth, newHeight);
+            return roatedSize;
+        }
+    }
+}
---
+++ Variables, Data, Expressions and Constants Homework/Variables, Data, Expressions and Constants/Task 2. PrintStatisticsInCSharp/Task 2. PrintStatisticsInCSharp/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Task_2.PrintStatisticsInCSharp
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            /// THING
+        }
+
+        public void PrintStatistics(double[] collection, int elementCount)
+        {
+            double minValue = double.MinValue;
+            double maxValue = double.MaxValue;
+            double sum = 0;
+
+            for (int i = 0; i < elementCount; i++)
+            {
+                if (collection[i] > maxValue)
+                {
+                    maxValue = collection[i];
+                }
+
+                if (collection[i] < minValue)
+                {
+                    minValue = collection[i];
+                }
+
+                sum += collection[i];
+            }
+
+            double averageSum = sum / elementCount;
+
+            this.PrintAverage(averageSum);
+            this.PrintMin(minValue);
+            this.PrintMax(maxValue);
+        }
+
+        private void PrintMax(double maxValue)
+        {
+            throw new NotImplementedException("TODO");
+        }
+
+        private void PrintMin(double minValue)
+        {
+            throw new NotImplementedException("TODO");
+        }
+
+        private void PrintAverage(double averageSum)
+        {
+            throw new NotImplementedException("TODO");
+        }
+    }
+}
